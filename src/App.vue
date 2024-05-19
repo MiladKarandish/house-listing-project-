9ea5d4db7c19e47893b43b0dc2713da220a7cc71
Ira Kaidanovich <startel2580startel@gmail.com>
2024-05-19 12:27:42 +0200

    Create header

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header  /> 
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0px;
+}
+
+body {
+  margin: 0px;
+}
+</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <div class="logo header_item">
+      <img src="@/assets/img_logo_dtt@3x.png" alt="Logo" /></div>
+    <div class="house_page header_item">House</div>
+    <div class="about_page header_item">About</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  color: rgb(31, 29, 29);
+  padding: 1em;
+  display: flex;
+  align-items:center;
+  padding: 0px;
+}
+
+.logo img {
+  height: 30px;
+}
+
+.header_item {
+  margin: 20px;
+}
+</style>
