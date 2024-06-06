703c8394f026c986a797d5d406710ac529db467a
Ira Kaidanovich <startel2580startel@gmail.com>
2024-06-06 09:35:24 +0200

    (Making House link active)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,18 +6,37 @@
       </div>
 
       <nav>
-        <router-link to="/" class="header-item">Houses</router-link>
-        <router-link to="/AboutPage" class="header-item">About</router-link>
+        <router-link
+          :to="{ name: 'HousePage' }"
+          :class="{ active: isHomeActive }"
+          >Houses</router-link
+        >
+        <router-link
+          :to="{ name: 'AboutPage' }"
+          :class="{ active: isAboutActive }"
+          >About</router-link
+        >
       </nav>
     </div>
   </header>
 </template>
 
-<script>
-export default {
-  name: "Header",
-  props: {},
-};
+<script setup>
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const router = useRoute();
+
+const isHomeActive = computed(() => {
+  const isActive =
+    router.name === "HousePage" || router.name === "HouseDetailsPage";
+  console.log("isHomeActive computed:", isActive);
+  return isActive;
+});
+
+const isAboutActive = computed(() => {
+  return router.name === "AboutPage";
+});
 </script>
 
 <style scoped>
@@ -45,6 +64,11 @@ export default {
   color: black;
 }
 
+.router-link-active, .active {
+  font-weight: bold;
+  color: black;
+}
+
 a {
   text-decoration: none;
   color: inherit;
@@ -52,6 +76,7 @@ a {
   font-family: "Montserrat";
   color: #c3c3c3;
   font-weight: 500;
+  margin: 0px 10px;
 }
 
 .header-content {
